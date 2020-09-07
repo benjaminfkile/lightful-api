3ac6811ddbab51f24b738d1519d22540f6d595df
ben <benjaminfkile@gmail.com>
2020-09-07 01:42:19 -0600

    add flag

--- src/GetLights/light-router.js
+++ src/GetLights/light-router.js
@@ -15,8 +15,8 @@ lightRouter
     .catch(next)
 })
 .post(jsonParser, (req, res, next) => {
-  const { lat, lng, url, upvotes, id } = req.body
-  const newLight = { lat, lng, url, upvotes ,id }
+  const { lat, lng, url, upvotes, id, flag } = req.body
+  const newLight = { lat, lng, url, upvotes ,id, flag }
 
   for (const [key, value] of Object.entries(newLight))
     if (value == null)
