17d533fec3e3df99de68ce193c07ce9337d18f67
ben <benjaminfkile@gmail.com>
2020-09-14 03:25:49 -0600

    more columns

--- src/GetLights/light-router.js
+++ src/GetLights/light-router.js
@@ -15,8 +15,8 @@ lightRouter
     .catch(next)
 })
 .post(jsonParser, (req, res, next) => {
-  const { lat, lng, url, upvotes, id, flag } = req.body
-  const newLight = { lat, lng, url, upvotes ,id, flag }
+  const { lat, lng, url, upvotes, id, flag, thumb, del } = req.body
+  const newLight = { lat, lng, url, upvotes ,id, flag, thumb, del }
 
   for (const [key, value] of Object.entries(newLight))
     if (value == null)
