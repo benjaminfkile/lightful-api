0f6036854a9f38ae18227ba429a09e37924c938b
ben <benjaminfkile@gmail.com>
2020-10-13 20:42:58 -0600

    stepping away from a queu, using sightengine to moderate photos

--- src/Lights/light-router.js
+++ src/Lights/light-router.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const axios = require('axios').default;
+const crypto = require("crypto");
 const LightService = require('./light-service')
 const lightRouter = express.Router()
+const sightengine = require('sightengine')('656105033', 'ELQmQagYWZyvoBkovEJ3');
+const jsonParser = express.json()
 
 lightRouter
   .route('/')
@@ -11,6 +15,34 @@ lightRouter
         res.json(Lights)
       })
       .catch(next)
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { lat, lng, url, id, user, del, upvotes } = req.body
+    let newLight = { lat, lng, url, id, user, del, upvotes }
+
+    sightengine.check(['nudity', 'wad', 'properties', 'offensive', 'faces', 'text-content', 'face-attributes', 'text']).set_url(newLight.url).then(function (result) {
+      let decision = LightService.auditLight(result)
+      if (!decision.denied) {
+        axios.post("https://api.imgbb.com/1/upload?key=eeadc880da3384d7927fb106962183a2&name=" + crypto.randomBytes(16).toString("hex") + "&image=" + newLight.url, {})
+          .then(res => {
+            console.log(res)
+            newLight.url = res.data.data.display_url
+            newLight.del = res.data.data.delete_url
+            LightService.insertLight(req.app.get('db'), newLight)
+          }).catch(function (error) {
+            console.log(error)
+            return res.status(400).json({
+              error: { message: `Server error` }
+            })
+          });
+      }
+      console.log(decision)
+    }).catch(function (err) {
+      console.log(err)
+      return res.status(400).json({
+        error: { message: `Server error` }
+      })
+    });
   })
 
 lightRouter
--- src/Lights/light-service.js
+++ src/Lights/light-service.js
@@ -1,11 +1,73 @@
 const service = {
 
   getAllLights(knex) {
-    return knex.from('lights').select('lat', 'lng', 'url', 'id','upvotes')
+    return knex.from('lights').select('lat', 'lng', 'url', 'id', 'upvotes')
   },
   getLightById(knex, id) {
     return knex.from('lights').select('*').where('id', id).first()
   },
+  insertLight(knex, newLight) {
+    return knex
+      .insert(newLight)
+      .into('lights')
+      .returning('*')
+      .then(rows => {
+        return rows[0]
+      })
+  },
+  auditLight(result) {
+    let decision = { denied: false }
+
+    if (result.weapon > .01) {
+      decision.weapon = "Found weapon",
+        decision.denied = true
+    }
+    if (result.alcohol > .01) {
+      decision.alcohol = "Found alcohol",
+        decision.denied = true
+    }
+    if (result.drugs > .01) {
+      decision.drugs = "Found drugs",
+        decision.denied = true
+    }
+    if (result.nudity.raw > .01) {
+      decision.nudity1 = "Found nudity",
+        decision.denied = true
+    }
+    if (result.nudity.partial > .01) {
+      decision.nudity2 = "Found nudity",
+        decision.denied = true
+    }
+    if (result.faces.length > 0) {
+      decision.faces = "Found human faces",
+        decision.denied = true
+    }
+    if (result.colors.dominant.r > 49 || result.colors.dominant.g > 49 || result.colors.dominant.b > 49) {
+      decision.colors = "Photo is too bright, make sure you are taking your photo in low light",
+        decision.denied = true
+    }
+    if (result.text.has_artificial > .01) {
+      decision.text0 = "Found artificial text",
+        decision.denied = true
+    }
+    if (result.text.profanity.length > 0) {
+      decision.text1 = "Found profane text",
+        decision.denied = true
+    }
+    if (result.text.personal.length > 0) {
+      decision.text2 = "Found personal text",
+        decision.denied = true
+    }
+    if (result.text.link.length > 0) {
+      decision.text3 = "Found links",
+        decision.denied = true
+    }
+    if (result.offensive.prob > .01) {
+      decision.offensive = "Found offensive material",
+        decision.denied = true
+    }
+    return decision
+  }
 }
 
 module.exports = service
