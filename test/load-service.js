c022c6d407d2ac2b150df3fa24b3c769c611868f
ben <benjaminfkile@gmail.com>
2020-11-28 02:11:14 -0700

    i dumb

--- test/load-service.js
+++ test/load-service.js
@@ -2,8 +2,7 @@ const loaderIoToken = 'loaderio-2aa50da43d3c2dc1193ac584e52d7cff'
 
 const service = {
     getToken(){
-        console.log(loaderIoToken + '/')
-        return loaderIoToken + '/'
+        return loaderIoToken
     }
 }
 
